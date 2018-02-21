16807f7bc5c44c71f4136ee188583f2a7ce71ee6
Dranderson <dranderson@DrandersondeMacBook-Air.local>
2018-02-21 14:49:49 -0600

    first commit

---
+++ build/src/CMakeFiles/menger.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/dranderson/Downloads/menger/src/camera.cc" "/Users/dranderson/Downloads/menger/build/src/CMakeFiles/menger.dir/camera.cc.o"
+  "/Users/dranderson/Downloads/menger/src/main.cc" "/Users/dranderson/Downloads/menger/build/src/CMakeFiles/menger.dir/main.cc.o"
+  "/Users/dranderson/Downloads/menger/src/menger.cc" "/Users/dranderson/Downloads/menger/build/src/CMakeFiles/menger.dir/menger.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/opt/local/include"
+  "../lib"
+  "/usr/local/Cellar/glfw/3.2.1/include"
+  "../lib/utgraphicsutil"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/dranderson/Downloads/menger/build/CMakeFiles/utgraphicsutil.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
